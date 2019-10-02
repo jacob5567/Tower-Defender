0ed63bae4391ceef273aad9f7a2dcc61fd75b87f
Jacob Faulk <jacob5567@gmail.com>
2019-10-02 16:44:13 -0700

    added gun to player character

---
+++ Assets/MyAssets/Scripts/GunshotScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunshotScript : MonoBehaviour
+{
+    private const int COOLDOWN = 10;
+    private AudioSource gunSound;
+    private Animation gunShotAnim;
+    private int cooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown = 0;
+        gunSound = GetComponent<AudioSource>();
+        gunShotAnim = GetComponent<Animation>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown--;
+        if (Input.GetButtonDown("Fire1") && cooldown <= 0)
+        {
+            gunSound.Play();
+            gunShotAnim.Play("Gunshot");
+            cooldown = COOLDOWN;
+        }
+    }
+}
